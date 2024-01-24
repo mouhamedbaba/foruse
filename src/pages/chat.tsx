91d7c978388579ecfe439de7b4db5d7fff02b051
mouhamed lamotte <lamottelymouhamed@gmail.com>
2024-01-24 04:20:02 +0000

    aside bar

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -9,7 +9,11 @@ const Chat = () => {
           <div className="w-1/12">
             <div className="h-full w-full bg-slate-900 dark:bg-white-200 rounded-3xl">
               <div className="py-3 px-4 h-full w-full flex flex-col gap-2 items-center">
-                <div className=" h-10 w-10 rounded-full"></div>
+                <div className=" h-10 w-10 rounded-full">
+                <svg viewBox="0 0 24 24" className="fill-red-600" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 3L13.4302 8.31181C13.6047 8.96 13.692 9.28409 13.8642 9.54905C14.0166 9.78349 14.2165 9.98336 14.451 10.1358C14.7159 10.308 15.04 10.3953 15.6882 10.5698L21 12L15.6882 13.4302C15.04 13.6047 14.7159 13.692 14.451 13.8642C14.2165 14.0166 14.0166 14.2165 13.8642 14.451C13.692 14.7159 13.6047 15.04 13.4302 15.6882L12 21L10.5698 15.6882C10.3953 15.04 10.308 14.7159 10.1358 14.451C9.98336 14.2165 9.78349 14.0166 9.54905 13.8642C9.28409 13.692 8.96 13.6047 8.31181 13.4302L3 12L8.31181 10.5698C8.96 10.3953 9.28409 10.308 9.54905 10.1358C9.78349 9.98336 9.98336 9.78349 10.1358 9.54905C10.308 9.28409 10.3953 8.96 10.5698 8.31181L12 3Z" className="stroke-red-600" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                </div>
                 <div className=" my-auto flex rounded-full grow flex-col justify-center  items-center gap-8">
                   <div className=" h-8 w-8">
                     <svg
@@ -97,11 +101,7 @@ const Chat = () => {
             <div className="flex flex-col h-full gap-2">
               <div className="bg-white dark:bg-slate-800  h-24 rounded-3xl">
                 <div className="py-3 px-4 h-full  w-full flex gap-2 items-center">
-                  {/* <div className="div">
-                    <h1 className="text-xl  font-bold  text-gray-700 dark:text-gray-200 sm:truncate ms-1 sm:tracking-tight">
-                      Chat
-                    </h1>
-                  </div> */}
+                  
                   <div className="div grow">
                     <input
                       type="text"
